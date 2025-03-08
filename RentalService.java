9c2aa4647c5cb2a6cbf2e24b675bfcc15496dfe2
vineeshah <vineeshah2006@gmail.com>
2025-03-08 13:56:56 -0800

    Update RentalService.java

--- RentalService.java
+++ RentalService.java
@@ -1,5 +1,19 @@
 package proj;
 
+import java.util.ArrayList;
+
 public class RentalService {
+	private ArrayList<Car> cinvent;
+	private ArrayList<Bike> binvent;
+	
+	public void addCar(Car car) {
+		cinvent.add(car);
+	}
+	public void addBike(Bike bike) {
+		binvent.add(bike);
+	}
+	
+	
+	
 
 }
