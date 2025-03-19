148901488bbd0888fccfaccf0c408cebd1b593ef
JorgeAZavala <jorge111zavala@gmail.com>
2025-03-18 20:27:43 -0700

    Update Main.java

--- src/project/Main.java
+++ src/project/Main.java
@@ -1,8 +1,11 @@
 package project;
 
 public class Main {
-	
-	private static class System {
-		
-	}
+    public static void main(String[] args) {
+        Store store = new Store("California", "SanJose");
+        Employee employee = new Employee("Vincent", 100, store);
+        Customer customer = new Customer("Kendrick", 85845, true, true, 0.1);
+        Vehicle vehicle = new Vehicle("VIN88754", "Toyota", "Corolla", 2022, 10000);
+	    
+    }
 }
