fc3a90da5c6f6ef3caed3b3cdc9e72b782d5dc39
JorgeAZavala <jorge111zavala@gmail.com>
2025-03-21 15:42:26 -0700

    Add files via upload

---
+++ src/project/Employee.java
@@ -0,0 +1,76 @@
+package project;
+
+
+public class Employee {
+    private final String name;
+    private final int id;
+    private final Store store;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Store getStore() {
+        return store;
+    }
+
+    public Employee(String name, int id, Store store) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Please select an employee. Try again.");
+        }
+        if (store == null) {
+            throw new IllegalArgumentException("Try again the store you are looking for did not appear. Please fill in the prompts.");
+        }
+        this.name = name;
+        this.id = id;
+        this.store = store;
+    }
+
+    public void processPayment(Order order) {
+    	
+        double total = order.getTotalAmountDue();
+        System.out.println(name + "your total amount was" + total );
+    }
+
+    public Order createOrder(Customer customer, Vehicle vehicle) {
+        if (customer == null) {
+            throw new IllegalArgumentException("Try again fill out your name please.");
+        }
+        if (vehicle == null) {
+            System.out.println("Try again tell us the model vehicle you are looking for");
+            return null;
+        }
+
+        Order order = new Order(customer, this, store);
+        System.out.println(name + " new order for" + customer.getName() + " customer id:" + customer.getMemberId() + " from " + store.getName());
+        return order;
+    }
+
+    public void applyDiscount(Order order, double discountRate) {
+    	
+        if (discountRate <= 0 || discountRate > 0.2) {
+            System.out.println("Invalid discount rate. No discount applied.");
+            return;
+        }
+
+        double newTotal = order.getTotalAmountDue() * (1 - discountRate);
+        order.setTotalAmountDue(newTotal);
+        System.out.println(name + " took off " + (discountRate * 100) + "% and your new total is : $" + newTotal);
+    }
+
+    public boolean validateCustomer(Customer customer) {
+
+        boolean activeMembership = customer.hasActiveMembership();
+        if (activeMembership) {
+            System.out.println(name + customer.getName() + "You qualify for our rentals");
+        } else {
+            System.out.println(name + customer.getName() + "Sorry you do not qualify for our rentals try again");
+        }
+        return activeMembership;
+    }
+    
+}
