17d4abfc0952c2ce994f62dc3963392bfc0ae01f
JorgeAZavala <jorge111zavala@gmail.com>
2025-03-21 00:54:29 -0700

    Update Customer.java

--- src/project/Customer.java
+++ src/project/Customer.java
@@ -7,7 +7,7 @@ public class Customer {
     private int memberId;
     private boolean membershipActive;
     private List<Vehicle> Rentable;
-
+    
     public Customer(String name, int memberId, boolean membershipActive) {
         public String getName() {
         return name;
@@ -29,7 +29,7 @@ public class Customer {
         this.membershipActive = membershipActive;
         this.Rentable = null;
     }
-
+    
     public void pay(pay order) {
         if (order == null) {
             throw new IllegalArgumentException("You have an empty order try again.");
@@ -43,18 +43,17 @@ public class Customer {
         }          
         
         double amount = order.getTotalAmount() * (1 - discount);
-        System.out.printf(name, amount, order.getOrderId());
+        System.out.println(name, amount, order.getOrderId());
     }
 
     public void rentVehicle(Store store, String vehicleModel) {
         if (store == null || vehicleModel == null || vehicleModel.trim().isEmpty()) {
             throw new IllegalArgumentException("The store you selected or car model was not found try again.");
         }
-        List<String> availableCarBrands = List.of(
-             "Toyota", "Honda", "Ford", "Chevrolet", "BMW", 
-             "Mercedes", "Audi", "Volkswagen", "Hyundai", "Kia");
+        
+        List<String> carModels = List.of( "Toyota", "Honda", "Ford", "Chevrolet", "BMW", "Mercedes", "Audi", "Volkswagen", "Hyundai", "Kia");
 
-        if (!availableCarBrands.contains(vehicleModel)) {
+        if (!carModels.contains(vehicleModel)) {
              System.out.println("The vehicle you are searching for is not avaible try again.");
              return;
         }
