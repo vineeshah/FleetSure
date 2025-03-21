38e2accdc3126bf9503493059542efb8f8d9f133
nguyen-james <jnguyen3852@gmail.com>
2025-03-21 10:39:54 -0700

    Order class methods
    Implementation of methods in the order class

--- src/project/Order.java
+++ src/project/Order.java
@@ -4,25 +4,49 @@ import java.util.ArrayList;
 public class Order {
 	private ArrayList<Vehicle> itemsInOrder;
 	private Customer customer;
-	double amountDue;
+	private double amountDue;
+	private Employee employee;
+	private Store location;
+	
+	//Constructor
+	public Order(Customer customer, Employee employee, Store location) {
+		this.customer = customer;
+		this.employee = employee;
+		this.location = location;
+		this.amountDue = 0;
+		this.itemsInOrder = new ArrayList<>();
+	}
 	
 	public double calculateTax() {
-		return 0;
+		return this.amountDue*.0725;
 	}
 	
-	public void addFees() {
-		
+	public void addFees(int amount) {
+		this.amountDue += amount;
 	}
 	
-	public void removeFees() {
-		
+	public void removeFees(int amount) {
+		this.amountDue -= amount;
 	}
 	
 	public void addToOrder(Vehicle vehicle) {
-		
+		itemsInOrder.add(vehicle);
 	}
 	
 	public void acceptPayment() {
-		
+		amountDue = 0;
+		System.out.println("The order has been paid for.");
+	}
+	
+	public double calculateAmountDue() {
+		for(Vehicle vehicle: itemsInOrder) {
+			if(vehicle instanceof Rentable) {
+				Rentable rental = (Rentable) vehicle;
+				amountDue += rental.generateRate()* rental.getDaysRented();
+			} else if (vehicle instanceof ForSale) {
+				
+				amountDue += vehicle.calculateValue();
+			}
+		}
 	}
 }
